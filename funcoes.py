257cbc490da0125631ee200cc0b8fc17de7366df
NicksName290 <nicolasname290@gmail.com>
2025-05-06 00:07:22 -0300

    aaaaa funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -5,4 +5,12 @@ def rolar_dados(qnt):
     valor = random.randint(1 , 6)
     lista.append(valor)
   return lista
-#PORQUE NN TA SALVANDO??
+
+def guardar_dado(dados_rolados, dados_no_estoque, dados_para_guardar):
+    resposta = []
+    for i in dados_para_guardar:
+        dados_no_estoque.append(dados_rolados[i])
+    rolar = rolar_dados(len(dados_rolados)-len(dados_para_guardar))
+    resposta.append(rolar)
+    resposta.append(dados_no_estoque)
+    return resposta
