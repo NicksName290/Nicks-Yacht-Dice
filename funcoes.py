66eb70297a254d0a63567eafc6ff71ec6f19b712
NicksName290 <nicolasname290@gmail.com>
2025-05-06 00:58:00 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -6,7 +6,7 @@ def rolar_dados(qnt):
     valor = random.randint(1 , 6)
     lista.append(valor)
   return lista
-#PELO AMOR ATUALIZA LOGO
+#PELO AMOR ATUALIZA LOGOOO
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     novo_estoque = dados_no_estoque + [dados_rolados[dado_para_guardar]]
     novos_dados = rolar_dados(len(dados_rolados) - 1)
