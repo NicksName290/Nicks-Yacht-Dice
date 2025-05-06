dabb4feb5a1b2ce22de32630ab4963a6515cb5bc
NicksName290 <nicolasname290@gmail.com>
2025-05-06 00:46:41 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-#versao : 5
+#versao : 6
 import random
 def rolar_dados(qnt):
   lista = []
@@ -10,8 +10,11 @@ def rolar_dados(qnt):
 def guardar_dado(dados_rolados, dados_no_estoque, dados_para_guardar):
     resposta = []
     dados_no_estoque.append(dados_rolados[dados_para_guardar])
-    rolar = rolar_dados(len(dados_rolados)-1)
-    resposta.append(rolar)
+    lista = []
+    for a in range(dados_rolados-1):
+      valor = random.randint(1 , 6)
+      lista.append(valor)
+    resposta.append(lista)
     resposta.append(dados_no_estoque)
     return resposta
-
+    
