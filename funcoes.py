52ec34054bb33c007b3ee5768eedb3ec70089f11
NicksName290 <nicolasname290@gmail.com>
2025-05-05 23:27:26 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,7 @@
 import random
 def rolar_dados(qnt):
   lista = []
-  i = 0
-  random = 0
-  while i != qnt:
-    random = random.randit(1 , 6)
-    lista.append(random)
+  for a in range(qnt)
+    valor = random.randit(1 , 6)
+    lista.append(valor)
   return lista
